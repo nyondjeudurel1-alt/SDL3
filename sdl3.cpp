b94a985880c3bc21e3b86106cf445e5051c510ff
nyondjeudurel1-alt <nyondjeudurel1@gmail.com>
2025-11-18 09:55:41 +0100

    Update sdl3.cpp

--- sdl3.cpp
+++ sdl3.cpp
@@ -1,4 +1,5 @@
 #include <SDL3/SDL.h>
+#include <iostream>
 
 #define WINDOW_HEIGHT 400
 #define WINDOW_WIDTH 640
@@ -9,14 +10,12 @@ int main()
     SDL_Window *Window = NULL;
     SDL_Renderer *Renderer = NULL;
 
-    /* SDL3 Initialisation */
     if (!SDL_Init(SDL_INIT_VIDEO))
     {
         SDL_Log("SDL_Init Error: %s", SDL_GetError());
         SDL_Quit();
     }
 
-    /* Window Creation */
     SDL_CreateWindowAndRenderer("My window", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &Window, &Renderer);
     if (Window == nullptr && Renderer == nullptr)
     {
@@ -24,7 +23,6 @@ int main()
         return -1;
     }
 
-    /* Game Loop */
     SDL_Event event;
     SDL_FRect rect;
     rect.x = rect.y = -1000;
@@ -32,7 +30,7 @@ int main()
     bool running = true;
     while (running)
     {
-        // Event handling
+    
         while (SDL_PollEvent(&event))
         {
             if (event.type == SDL_EVENT_QUIT || event.key.scancode == SDL_SCANCODE_ESCAPE)
@@ -51,7 +49,7 @@ int main()
                 case SDL_SCANCODE_DOWN:
                     rect.y += 25;
                     break;
-                case SDL_SCANCODE_UP:
+                case SDL_SCANCODE_UP: 
                     rect.y -= 25;
                     break;
                 default:
@@ -67,16 +65,15 @@ int main()
                 break;
             }
         }
-        // main loop
+
         SDL_SetRenderDrawColor(Renderer, 0, 125, 0, 255);
         SDL_RenderClear(Renderer);
 
-        SDL_SetRenderDrawColor(Renderer, 255, 255, 255, 255); // Fill Rectangle
+        SDL_SetRenderDrawColor(Renderer, 0, 0, 255, 80); 
         SDL_RenderFillRect(Renderer, &rect);
         SDL_RenderPresent(Renderer);
     }
 
-    /* SDL3 Exit */
     SDL_DestroyRenderer(Renderer);
     SDL_DestroyWindow(Window);
     SDL_Quit();
